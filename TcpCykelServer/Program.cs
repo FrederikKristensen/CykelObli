44adb4fe1a130b84fe36884267bc3e537c09afc0
Frederik Kristensen <fred009a@edu.easj.dk>
2020-09-30 17:14:25 +0200

    Tcp done

---
+++ TcpCykelServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TcpCykelServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerWorker worker = new ServerWorker();
+            worker.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TcpCykelServer/ServerWorker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using CykelObli;
+
+namespace TcpCykelServer
+{
+    class ServerWorker
+    {
+        public void Start()
+        {
+            // creating server, with port 4646
+            TcpListener server = new TcpListener(IPAddress.Loopback, 4646);
+            
+            // starting server
+            server.Start();
+
+
+            // waiting on client to make a connection
+            TcpClient socket = server.AcceptTcpClient();
+
+            StreamReader sr = new StreamReader(socket.GetStream());
+            StreamWriter sw = new StreamWriter(socket.GetStream());
+
+            // read text from client
+            string str = sr.ReadLine();
+            
+            // server writing back
+            Console.WriteLine($"This is the server input: {str}");
+
+            //flushing the buffer
+            sw.Flush();
+
+            socket.Close();
+        }
+    }
+}
