1dd57e2879a509c1302d9f91c6ac65bd8c26dfc3
Frederik Kristensen <fred009a@edu.easj.dk>
2020-09-30 17:16:19 +0200

    Test and rest done

---
+++ CykTest/CykTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CykelObli;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CykTest
+{
+    [TestClass]
+    public class CykTests
+    {
+        private CykelClass.Cykel cy1;
+
+        [TestInitialize()]
+        public void TestInitializer()
+        {
+            cy1 = new CykelClass.Cykel(2,"Gul",200,23); 
+        }
+
+        [TestMethod]
+        public void CykelConstructor()
+        {
+            Assert.AreEqual(2,cy1.Id);
+            Assert.AreEqual("Gul",cy1.Farver);
+            Assert.AreEqual(200,cy1.Pris);
+            Assert.AreEqual(23,cy1.Gear);
+        }
+
+        [TestMethod()]
+        public void CykId()
+        {
+            cy1.Id = 2;
+        }
+
+        [TestMethod()]
+        public void CykFarver()
+        {
+            cy1.Farver = "Gul";
+        }
+
+        [TestMethod()]
+        public void CykPris()
+        {
+            cy1.Pris = 200;
+        }
+
+        [TestMethod()]
+        public void CykGear()
+        {
+            cy1.Gear = 23;
+        }
+    }
+}
---
+++ RestCykel/Controllers/CykelController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CykelObli;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestCykel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CykelController : ControllerBase
+    {
+        // Data
+        private static List<CykelClass.Cykel> _data = new List<CykelClass.Cykel>()
+        {
+            new CykelClass.Cykel(1, "Red", 300, 24),
+            new CykelClass.Cykel(2, "Blue", 240, 12),
+            new CykelClass.Cykel(3, "Green", 200, 6)
+        };
+
+        // GET: api/Cykel
+        [HttpGet]
+        public IEnumerable<CykelClass.Cykel> Get()
+        {
+            return _data;
+        }
+
+        // GET: api/Cykel/5
+        [HttpGet("{id}")]
+        public CykelClass.Cykel Get(int id)
+        {
+            return _data.Find(c => c.Id == id);
+        }
+
+        // POST: api/Cykel
+        [HttpPost]
+        public void Post([FromBody] CykelClass.Cykel value)
+        {
+            _data.Add(value);
+        }
+
+        // PUT: api/Cykel/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            // coming
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            CykelClass.Cykel cykel = Get(id);
+            if (cykel != null)
+            {
+                _data.Remove(cykel);
+            }
+        }
+    }
+}
